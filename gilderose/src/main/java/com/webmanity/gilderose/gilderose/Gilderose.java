f10e85a80a8de293a0e57bc33a7adfdbbd3e78e4
youne <younesturki@live.fr>
2020-05-03 14:49:17 +0200

    Moving somes conditions + Test OK

--- gilderose/src/main/java/com/webmanity/gilderose/gilderose/Gilderose.java
+++ gilderose/src/main/java/com/webmanity/gilderose/gilderose/Gilderose.java
@@ -14,13 +14,13 @@ class GildedRose {
         for (Item item : items) {
 			if (!item.name.equals(AGED_BRIE)
                     && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+				if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+					if (item.quality > 0 && item.sellIn > 0) {
                     	item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (item.quality < 50) {
+                if (item.quality < 50 && item.sellIn > 0) {
                 	item.quality = item.quality + 1;
 
                     if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
@@ -44,18 +44,17 @@ class GildedRose {
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
+                	 if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                        if (item.quality > 0 && item.sellIn < 0) {
                             	item.quality = item.quality - 1;
                             }
-                        }
-                    } else {
-                    	item.quality = item.quality - item.quality;
+                         else {
+                        	 item.quality = item.quality - item.quality;
+                         }
                     }
                 } else {
-                    if (item.quality < 50) {
+                    if (item.quality < 50 && item.sellIn < 0) {
                     	item.quality = item.quality + 1;
                     }
                 }
